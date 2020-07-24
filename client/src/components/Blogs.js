b1c4b526d710d21084245f720aeced892a803466
abechoi <abraham.choi@icloud.com>
2020-07-23 22:01:30 -0700

    Added a blog

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -1,28 +1,24 @@
 import React from "react";
+import blogs  from "./data/blogs";
 
 const Blogs = () => {
-  return (
-    <section className="main-section">
-      FIRST<br/>
-      BlOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BlOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BlOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      BlOGS<br/>
-      BLOGS<br/>
-      BLOGS<br/>
-      LAST<br/>
+
+
+  return blogs.length ? (
+    <section>
+      <ul className="blog-list">
+        { blogs.map( blog => {
+          return (
+            <li key={ blog.id }>
+              <h1>{ blog.title }</h1>
+              <p>{ blog.body }</p>
+            </li>);
+        })}
+      </ul>
+    </section>
+  ) : (
+    <section>
+      <p>No available blogs.</p>
     </section>
   );
 }
