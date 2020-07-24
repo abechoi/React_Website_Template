e470bed5ce7b5d1f4c85a4f9a841b82c5fae7332
abechoi <abraham.choi@icloud.com>
2020-07-24 02:06:09 -0700

    Formatted Blogs

--- client/src/components/Blogs.js
+++ client/src/components/Blogs.js
@@ -5,19 +5,34 @@ const Blogs = () => {
 
 
   return blogs.length ? (
-    <section>
-      <ul className="blog-list">
-        { blogs.map( blog => {
-          return (
-            <li key={ blog.id }>
-            <div className="blog-card">
-              <h1>{ blog.title }</h1>
-              <p>{ blog.paragraph1 }</p>
-              <p>{ blog.paragraph2 }</p>
-            </div>
-            </li>);
-        })}
-      </ul>
+    <section className="blogs-section">
+      <div className="blogs-div">
+        <ul>
+          { blogs.map( blog => {
+            return (
+              <li key={ blog.id }>
+              <div className="blog-card">
+
+                <h1 className="blog-title">{ blog.title }</h1>
+                <p className="blog-date">{ blog.createdAt }</p>
+
+                <div>{ blog.content.map(i => {
+                  return <p className="blog-content" key={blog.content.indexOf(i)}><br/>{i}<br/></p>;
+                })}</div>
+
+              </div>
+              </li>);
+          })}
+        </ul>
+      </div>
+      <div className="title-div">
+        <h4>RECENT POSTS</h4>
+        <ul>
+          { blogs.map(blog => {
+            return <p key={blog.id}>{ blog.title }</p>;
+          })}
+        </ul>
+      </div>
     </section>
   ) : (
     <section>
