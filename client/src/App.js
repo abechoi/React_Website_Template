b78ed81e50ac28f9e5f1fbbd84c6829d4ff7556b
abechoi <abraham.choi@icloud.com>
2020-07-30 20:54:56 -0700

    Fixed project routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,13 @@ import Blogs from './components/Blogs';
 import Docs from './components/Docs';
 import About from './components/About';
 import Projects from './components/Projects';
+import projects from "./components/data/projects";
+import ProjectDetails from "./components/ProjectDetails";
 
 const App = () => {
   return (
     <div className="App">
+
       <Router>
         <Navbar/>
         <Switch>
@@ -17,9 +20,17 @@ const App = () => {
           <Route path="/blogs"><Blogs/></Route>
           <Route path="/docs"><Docs/></Route>
           <Route path="/projects"><Projects/></Route>
+          { projects.map((project) => (
+
+            <Route path={project.url}>
+              <ProjectDetails project={project} key={project.id}/>
+            </Route>
+            
+          ))}
         </Switch>
         <Footer/>
       </Router>
+
     </div>
   );
 }
--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,26 +1,15 @@
-import React from "react";
+import React from 'react';
 import projects from "./data/projects";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
-
-  return projects.length ? (
-    <section className="projects-section">
-      <ul>
-        { projects.map(project => (
-          <li key={project.id}>
-            <div className="projects-card">
-              <img className="projects-thumbnail" src={ require(project.thumbnail+"") } alt="projects-thumbnail"/>
-              <h1 className="projects-title">{ project.title }</h1>
-              <p>{ project.createdAt }</p>
-              <p>{ project.info }</p>
-            </div>
-          </li>
+  return (
+    <section>
+      <ul className="projects-grid">
+        { projects.map((project) => (
+          <ProjectCard project={project} key={project.id}/>
         ))}
       </ul>
-    </section>
-  ) : (
-    <section className="empty-section">
-      <h3>Projects are coming soon!</h3>
     </section>
   );
 }
