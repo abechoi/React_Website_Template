979ebfd417241b586f665a618410ebadf2400cd6
abechoi <abraham.choi@icloud.com>
2020-08-04 16:17:41 -0700

    Added CustomComponent.js

--- client/src/components/data/projects.js
+++ client/src/components/data/projects.js
@@ -1,7 +1,8 @@
 import { v4 } from "uuid";
+import AbeChoiApp from '../apps/AbeChoiApp';
 
 const projects = [
-  { id: v4(), title: 'ABECHOI.COM', url: '/abechoi', demo: "AbeChoiApp", createdAt: "July 29, 2020", updatedAt: "July 31, 2020",
+  { id: v4(), title: 'ABECHOI.COM', url: '/abechoi', demo: AbeChoiApp, createdAt: "July 29, 2020", updatedAt: "July 31, 2020",
   thumbnail: "./images/projects-thumbnail-1.png",
   info: "This website was made in React (see flavicon at the tab if you don't believe me!). This site will showcase my future React apps and I will also create and upload documents on solutions that I will need.",
   sections: [
